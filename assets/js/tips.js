08657fedd0558eec26881101d257f965a863dc29
juljeryt <juljeryt@gmail.com>
2025-04-06 07:47:14 +0000

    show content on click with cool fade in animation

---
+++ assets/js/tips.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.square').forEach(square => {
+    square.addEventListener('click', function () {
+        const textId = this.getAttribute('data-text-id'); // get id of the hidden text
+        const hiddenText = document.getElementById(textId); // find hidden text
+
+        if (hiddenText) {
+            this.style.backgroundImage = 'none'; // hide bg image
+            this.textContent = hiddenText.textContent; // set text content
+            this.style.textAlign = 'center'; // later we can change this to left
+            this.style.padding = '10px'; // padding for better look
+            this.style.fontSize = '18px'; // and changed font size bcs we dont want our text to be 50px lmaoo
+
+            // add fade in effect
+            this.classList.add('fade-in');
+        }
+    });
+});
